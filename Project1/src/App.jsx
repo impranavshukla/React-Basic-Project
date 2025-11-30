2fa3d044b31df49cff62ed80d0cc2b06f61f7dde
Pranav Shukla <impranavshukla@gmail.com>
2025-11-30 23:29:51 +0530

    WeatherApp project added

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -1,21 +1,29 @@
 
+import { useState } from 'react';
 import './App.css'
 import AllCards from './Components/AllCards'
 import Heading from './Components/Heading'
+import data from './utils/data';
+
+
+
 
 function App() {
+  const [val,setVal]=useState("");
   function filterCards(){
-    console.log("Filter button clicked");
+    console.log(val);
   }
 
   return (
     <>
     <Heading/>
     <div>
-      <input type="text" placeholder='Kya chahiye ??...' />
+      <input type="text" placeholder='Kya chahiye ??...' onChange={function (e){
+        setVal(e.target.val)
+        }}/>
       <button onClick={filterCards}>Search</button>
     </div>
-    <AllCards/>
+    <AllCards data={data}/>
     </>
   )
 }
